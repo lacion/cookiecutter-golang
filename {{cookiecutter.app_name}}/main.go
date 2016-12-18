74e3201003bb8554a0a9dd8efde44432280e6f5b
Luis Morales <luis@dronedeploy.com>
2016-12-18 12:22:03 -0800

    added basic version info to the go binary

--- {{cookiecutter.app_name}}/main.go
+++ {{cookiecutter.app_name}}/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
+
+	versionFlag := flag.Bool("version", false, "Version")
+	flag.Parse()
+
+	if *versionFlag {
+		fmt.Println("Git Commit:", GitCommit)
+		fmt.Println("Version:", Version)
+		if VersionPrerelease != "" {
+			fmt.Println("Version PreRelease:", VersionPrerelease)
+		}
+	}
+
 	fmt.Println("Hello.")
 }
