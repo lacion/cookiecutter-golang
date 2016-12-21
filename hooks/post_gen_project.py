0205d0eb308df40dfdb72a116103ab601bb81d4d
Luis Morales <luis@dronedeploy.com>
2016-12-21 14:17:28 +0100

    cleaned up if git not being used

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,11 +4,20 @@ Does the following:
 1. Inits git if used
 2. Deletes dockerfiles if not going to be used
 """
+from __future__ import print_function
 import os
 from subprocess import Popen
 
 # Get the root project directory
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+
+def remove_file(filename):
+    """
+    generic remove file from project dir
+    """
+    fullpath = os.path.join(PROJECT_DIRECTORY, filename)
+    if os.path.exists(fullpath):
+        os.remove(fullpath)
 
 def init_git():
     """
@@ -19,6 +28,7 @@ def init_git():
         ["git", "add", "."],
         ["git", "commit", "-a", "-m", "Initial Commit."]
     ]
+
     for command in GIT_COMMANDS:
         git = Popen(command, cwd=PROJECT_DIRECTORY)
         git.wait()
@@ -33,10 +43,12 @@ def remove_docker_files():
             PROJECT_DIRECTORY, filename
         ))
 
-# 1. Initialize Git
-if '{{ cookiecutter.use_docker }}'.lower() != 'y':
-    remove_docker_files()
-
 # 1. Remove Dockerfiles if docker is not going to be used
 if '{{ cookiecutter.use_docker }}'.lower() != 'y':
     remove_docker_files()
+
+# 2. Initialize Git (should be run after all file have been modified or deleted)
+if '{{ cookiecutter.use_git }}'.lower() == 'y':
+    init_git()
+else:
+    remove_file(".gitignore")
